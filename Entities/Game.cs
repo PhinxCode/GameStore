e2af5d2cbec449ee74e40517a427982493affe76
Null <phinxcode@gmail.com>
2025-01-20 18:21:22 -0400

    entities

---
+++ Entities/Game.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace GameStore.Api.Entities;
+
+public class Game
+{
+
+}
---
+++ Entities/Genre.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace GameStore.Api.Entities;
+
+public class Genre
+{
+    public int Id { get; set;}
+    public string Name { get; set;}  = String.Empty
+
+}
